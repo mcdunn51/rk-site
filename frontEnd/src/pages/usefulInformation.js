ffd15287cf58d33763e734ed8a368c303d914578
Matt Dunning <Matt.dunning@SVG.local>
2019-04-01 17:07:12 +0100

    general homepage edits

---
+++ frontEnd/src/pages/usefulInformation.js
@@ -0,0 +1,23 @@
+// import React, { Component } from 'react'
+// import SideNavigationBar from '../Components/usefulInformation/SideNavigationBar'
+
+// import AboutUs from '../Components/usefulInformation/AboutUs'
+// import ContactUs from '../Components/usefulInformation/ContactUs'
+// import DeliveryInformation from '../Components/usefulInformation/DeliveryInformation'
+// import PrivacyAndCookiePolicy from '../Components/usefulInformation/PrivacyAndCookiePolicy'
+// import ReturnsAndCancelationPolicy from '../Components/usefulInformation/ReturnsAndCancelationPolicy'
+// import Showroom from '../Components/usefulInformation/Showroom'
+// import SiteTermsAndConditions from '../Components/usefulInformation/SiteTermsAndConditions'
+// import TradeAccountRegistration from '../Components/usefulInformation/TradeAccountRegistration'
+
+// class UsefulInformation extends Component {
+//     render () {
+//         return (
+
+//         )
+//     }
+// }
+
+// export default UsefulInformation;
+
+
