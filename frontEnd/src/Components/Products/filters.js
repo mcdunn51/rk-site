2c36578213e57f3b4977743a17f537b411ab29f9
Matt Dunning <Matt.dunning@SVG.local>
2019-04-12 17:03:09 +0100

    working on filters dropdown

--- frontEnd/src/Components/Products/filters.js
+++ frontEnd/src/Components/Products/filters.js
@@ -1,24 +1,31 @@
-import React, {Component} from 'react';
-import { Col } from 'react-bootstrap';
+import React, { Component } from 'react';
+// import { Col } from 'react-bootstrap';
 
 class Filters extends Component {
-    render () {
-    return (
-        <Col md={2} id="filters">
-        <h3>Filters</h3>
-        <h3>Brand</h3>
-        <h3>Product Category</h3>
-        <h3>Colour</h3>
-        <h3>Price</h3>
-        <h3>Minimum available qty</h3> 
-            <ul id="filtersList">
-                <li><input type="checkbox"></input>red</li>
-                <li><input type="checkbox"></input>green</li>
-                <li><input type="checkbox"></input>blue</li>
-                <li><input type="checkbox"></input>pink</li>
-            </ul>
-        </Col>
-    )} 
-}
-
-export default Filters;
+    render() {
+        return (
+            <form id="filters">
+                <div className="filterWrapper">
+                    <div>
+                        <h3>BRAND</h3>
+                        -
+                    </div>
+                    <ul class="filtersList">
+                        <li><input type="checkbox"></input>red</li>
+                        <li><input type="checkbox"></input>green</li>
+                        <li><input type="checkbox"></input>blue</li>
+                        <li><input type="checkbox"></input>pink</li>
+                    </ul>
+                </div>
+                    <div>
+                        <h3>PRODUCT CATEGORY</h3>
+                        <h3>COLOUR</h3>
+                        <h3>PRICE</h3>
+                        <h3>MINIMUM AVAILABLE QTY</h3>
+                    </div>
+            </form>
+                    )
+                }
+            }
+            
+            export default Filters;
