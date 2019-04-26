92025a5ef098d3e1a63ef9263f28587a651cd848
Matt Dunning <Matt.dunning@SVG.local>
2019-04-26 15:14:48 +0100

    working on main account page

--- frontEnd/src/Components/Account/index.js
+++ frontEnd/src/Components/Account/index.js
@@ -1,59 +1,191 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, Card, Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhone, faEnvelope, faMobileAlt } from '@fortawesome/free-solid-svg-icons'
 
 class Index extends Component {
     render() {
         return (
             <Container id="accountIndexWrapper">
-                <Row>
-                    <Col>
+                <Row className="mb-5">
+                    <Col id="firstCol">
                         <h2>Hello David Blyth,</h2>
-                        <div>
+                        <div id="customerNumberWrapper">
                             Customer Number: ?
                         </div>
                     </Col>
                 </Row>
-                <Row>
+                <Row className="mb-5">
                     <Col>
                         <Button variant="primary">Orders</Button>
                         <Button variant="primary">Addresses</Button>
                         <Button variant="primary">Logout</Button>
                     </Col>
                 </Row>
-                <Row>
+                <Row className="mb-5">
                     <Col>
                         <Card>
-                            <Card.Title>Card Title</Card.Title>
+                            <div className="cardTitleWrapper">
+                                <Card.Title>Card Title</Card.Title>
+                            </div>
                             <Card.Body>
+                                <div className="nameWrapper">
+                                    Matthew Dunning
+                                </div>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faPhone} />
+                                        +44 7526745507
+                                    </a>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Text>
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faMobileAlt} />
+                                        +44 7526745507
+                                    </a>
+                                </Card.Text>
+                                <Card.Text>
+                                    <a href="mailto:webmaster@example.com">
+                                        <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                                        webmaster@example.com
+                                    </a>
+                                </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col>
                         <Card>
-                            <Card.Title>Card Title</Card.Title>
+                            <div className="cardTitleWrapper">
+                                <Card.Title>Card Title</Card.Title>
+                            </div>
                             <Card.Body>
+                                <div className="nameWrapper">
+                                    Matthew Dunning
+                                </div>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faPhone} />
+                                        +44 7526745507
+                                    </a>
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Text>
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faMobileAlt} />
+                                        +44 7526745507
+                                    </a>
+                                </Card.Text>
+                                <Card.Text>
+                                    <a href="mailto:webmaster@example.com">
+                                        <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                                        webmaster@example.com
+                                    </a>
+                                </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col>
                         <Card>
-                            <Card.Title>Card Title</Card.Title>
+                            <div className="cardTitleWrapper">
+                                <Card.Title>Card Title</Card.Title>
+                            </div>
                             <Card.Body>
+                                <div className="nameWrapper">
+                                    Matthew Dunning
+                                </div>
+                                <Card.Text>
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faPhone} />
+                                        +44 7526745507
+                                    </a>
+                                </Card.Text>
+                                <Card.Text>
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faMobileAlt} />
+                                        +44 7526745507
+                                    </a>
+                                </Card.Text>
+                                <Card.Text>
+                                    <a href="mailto:webmaster@example.com">
+                                        <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                                        webmaster@example.com
+                                    </a>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className="mb-5">
+                    <Col>
+                        <Card>
+                            <div className="cardTitleWrapper">
+                                <Card.Title>Card Title</Card.Title>
+                            </div>
+                            <Card.Body>
+                                <div className="nameWrapper">
+                                    Matthew Dunning
+                                </div>
+                                <Card.Text>
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faPhone} />
+                                        +44 7526745507
+                                    </a>
+                                </Card.Text>
+                                <Card.Text>
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faMobileAlt} />
+                                        +44 7526745507
+                                    </a>
+                                </Card.Text>
+                                <Card.Text>
+                                    <a href="mailto:webmaster@example.com">
+                                        <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                                        webmaster@example.com
+                                    </a>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <div className="cardTitleWrapper">
+                                <Card.Title>Card Title</Card.Title>
+                            </div>
+                            <Card.Body>
+                                <div className="nameWrapper">
+                                    Matthew Dunning
+                                </div>
+                                <Card.Text>
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faPhone} />
+                                        +44 7526745507
+                                    </a>
+                                </Card.Text>
+                                <Card.Text>
+                                    <a href="tel:+44 7526745507">
+                                        <FontAwesomeIcon className="icon" icon={faMobileAlt} />
+                                        +44 7526745507
+                                    </a>
+                                </Card.Text>
+                                <Card.Text>
+                                    <a href="mailto:webmaster@example.com">
+                                        <FontAwesomeIcon className="icon" icon={faEnvelope} />
+                                        webmaster@example.com
+                                    </a>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
                                 <Card.Text>
                                     Some quick example text to build on the card title and make up the bulk of
                                     the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
+                                <Card.Link href="#">Card Link</Card.Link>
+                                <Card.Link href="#">Another Link</Card.Link>
                             </Card.Body>
                         </Card>
                     </Col>
